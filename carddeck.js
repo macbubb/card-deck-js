a7bd908d6bb977314cdfb220899fec226dabf37b
Ernest Bubb <bubbem@gmail.com>
2019-04-12 13:26:34 -0700

    tiddied up my experiment. created seperate Deck object

--- carddeck.js
+++ carddeck.js
@@ -1,25 +1,26 @@
-
-//card object. suit, number
-//deck object
+function Deck() {
+    this.stack = [];
+}
 
 function Card(value, suit) {
     this.value = value;
     this.suit = suit;
-    this.stack = [];
 }
 
 Card.prototype.announce = function() {
     return this.value + " of " + this.suit;
 }
 
-Card.prototype.makeStack = function() {
+//this seems like it should be considered the constructor, but in JS the constructor seems limited to just assigning variables names / values from arguments
+Deck.prototype.makeStack = function() {
     if (this.stack.length === 0) {
-        console.log("making deck");
+        console.log("Making deck...");
+
         let suits = ["Hearts","Diamonds","Spades","Clubs"];
         let faceCards = ["Jack", "Queen", "King", "Ace"];
 
         for (let i = 0; i < 4; i++) {
-            for (let j = 1; j < 11; j++) {
+            for (let j = 2; j < 11; j++) {
                 this.stack.push([j,suits[i]]);
             }
             for (let n = 0; n < 4; n++) {
@@ -29,20 +30,23 @@ Card.prototype.makeStack = function() {
     }
 }
 
-/*
-var randomCard = function(deck) {
-    return deck[Math.floor(Math.random() * 52)];
+Deck.prototype.randomCard = function() {
+    return this.stack[Math.floor(Math.random() * 52)];
 }
 
+
+
+
+/*
 for (var n = 0; n < 10;n++) {
-   console.log(randomCard(deck2));
+   console.log(randomCard());
 }
 */
 
-var asdf = new Card('Ace', 'Spades');
+var redDeck = new Deck();
 
-asdf.makeStack();
+redDeck.makeStack();
 
-console.log(asdf.announce());
-console.table(asdf.stack);
+console.log(redDeck.randomCard());
+
 //console.log(deck2[5].announce); 
